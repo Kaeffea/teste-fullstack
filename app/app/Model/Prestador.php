cf11cfb34594ff7530d3717a13c3807930c6894f
kaeffea <kaeffea@gmail.com>
2025-12-06 20:06:48 +0100

    feat(database): cria estrutura do banco de dados com relacionamento N:N

---
+++ app/app/Model/Prestador.php
@@ -0,0 +1,58 @@
+<?php
+App::uses('AppModel', 'Model');
+
+/**
+ * Model Prestador
+ * Representa os prestadores de serviço cadastrados
+ */
+class Prestador extends AppModel {
+    
+    public $name = 'Prestador';
+    
+    // Campos que serão preenchidos automaticamente
+    public $actsAs = array('Containable');
+    
+    // Validações
+    public $validate = array(
+        'nome' => array(
+            'notEmpty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'O nome é obrigatório'
+            )
+        ),
+        'sobrenome' => array(
+            'notEmpty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'O sobrenome é obrigatório'
+            )
+        ),
+        'email' => array(
+            'email' => array(
+                'rule' => 'email',
+                'message' => 'Por favor, insira um email válido'
+            ),
+            'unique' => array(
+                'rule' => 'isUnique',
+                'message' => 'Este email já está cadastrado'
+            )
+        ),
+        'telefone' => array(
+            'notEmpty' => array(
+                'rule' => 'notEmpty',
+                'message' => 'O telefone é obrigatório'
+            )
+        )
+    );
+    
+    // Relacionamento
+    public $hasAndBelongsToMany = array(
+        'Servico' => array(
+            'className' => 'Servico',
+            'joinTable' => 'prestadores_servicos',
+            'foreignKey' => 'prestador_id',
+            'associationForeignKey' => 'servico_id',
+            'with' => 'PrestadorServico',
+            'unique' => true
+        )
+    );
+}
